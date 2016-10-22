e2944760e8b833dc04da7c10c40ae819430acbdc
mustafabhatkar <mustafabhatkar@gmail.com>
2016-10-22 15:46:07 +0530

    added tabs

--- app/src/main/java/com/apptreak/convertx/MainActivity.java
+++ app/src/main/java/com/apptreak/convertx/MainActivity.java
@@ -1,25 +1,47 @@
 package com.apptreak.convertx;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.EditText;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-private EditText txtInput;
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtInput = (EditText) findViewById(R.id.txtInput);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Length"));
+        tabLayout.addTab(tabLayout.newTab().setText("Area"));
+        tabLayout.addTab(tabLayout.newTab().setText("Temperature"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnCmOp:
 
-                break;
-        }
-    }
 }
