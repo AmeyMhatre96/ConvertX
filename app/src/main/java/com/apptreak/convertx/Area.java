7a0508cce2138809a6a99b220964f399142b532a
mustafabhatkar <mustafabhatkar@gmail.com>
2016-10-22 16:35:53 +0530

    Add files via upload

---
+++ app/src/main/java/com/apptreak/convertx/Area.java
@@ -0,0 +1,21 @@
+package com.apptreak.convertx;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Farman on 10/22/2016.
+ */
+
+public class Area extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.area_layout,container,false)
+                ;
+    }
+}
---
+++ app/src/main/java/com/apptreak/convertx/PagerAdapter.java
@@ -0,0 +1,40 @@
+package com.apptreak.convertx;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Farman on 10/22/2016.
+ */
+
+public class PagerAdapter extends FragmentPagerAdapter {
+    int noOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int numOfTabs) {
+        super(fm);
+        this.noOfTabs = numOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                Length length = new Length();
+                return length;
+            case 1:
+                Area area = new Area();
+                return area;
+            case 2:
+                Temperature temperature = new Temperature();
+                return temperature;
+         default:return null;
+        }
+    }
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
