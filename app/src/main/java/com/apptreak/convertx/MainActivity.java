58ce631479b08780e5f918da1e47d9c54f61157d
mustafabhatkar <mustafabhatkar@gmail.com>
2016-11-01 01:06:03 +0530

    set default txt to op using fab

--- app/src/main/java/com/apptreak/convertx/MainActivity.java
+++ app/src/main/java/com/apptreak/convertx/MainActivity.java
@@ -5,30 +5,22 @@ import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.app.Fragment;
 
-import static android.R.attr.fragment;
-import static android.R.attr.text;
-
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     Context context;
 
 
-    EditText textIn ;
+    EditText textIn;
+    TextView textOp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,112 +54,122 @@ public class MainActivity extends AppCompatActivity  {
         viewPager.setAdapter(adapter);
 
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                switch (tabLayout.getSelectedTabPosition()) {
 
 
-            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
+                    case 0:
+                        textIn = (EditText) findViewById(R.id.txtInputLength);
+                        textOp = (TextView) findViewById(R.id.txtOpLength);
+                        textIn.getText().clear();
+                        textOp.setText(R.string.txt_default);
+                        break;
+                    case 1:
+                        textIn = (EditText) findViewById(R.id.txtInputArea);
+                        textOp = (TextView) findViewById(R.id.txtOpArea);
+                        textIn.getText().clear();
+                        textOp.setText(R.string.txt_default);
+                        break;
+                    case 2:
+                        textIn = (EditText) findViewById(R.id.tempInput);
+                        textOp = (TextView) findViewById(R.id.tempOut);
+                        textIn.getText().clear();
+                        textOp.setText(R.string.txt_default);
+                        break;
+                    case 3:
+                        textIn = (EditText) findViewById(R.id.textWeightInput);
+                        textOp = (TextView) findViewById(R.id.textWeightOut);
+                        textIn.getText().clear();
+                        textOp.setText(R.string.txt_default);
+                        break;
+                    case 4:
+                        textIn = (EditText) findViewById(R.id.timeInput);
+                        textOp = (TextView) findViewById(R.id.timeOutput);
+                        textIn.getText().clear();
+                        textOp.setText(R.string.txt_default);
+                        break;
+                    case 5:
+                        textIn = (EditText) findViewById(R.id.textVolumeInput);
+                        textOp = (TextView) findViewById(R.id.textVolumeOut);
+                        textIn.getText().clear();
+                        textOp.setText(R.string.txt_default);
+                        break;
+                }
 
-                    switch (tabLayout.getSelectedTabPosition()) {
+
+            }
+        });
+        fab.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
 
 
-                        case 0:
-                            textIn = (EditText) findViewById(R.id.txtInputLength);
-                            textIn.setText("");
-                            break;
-                        case 1:
-                            textIn = (EditText) findViewById(R.id.txtInputArea);
-                            textIn.setText("");
-                            break;
-                        case 2:
-                            textIn = (EditText) findViewById(R.id.tempInput);
-                            textIn.setText("");
-                            break;
-                        case 3:
-                            textIn = (EditText) findViewById(R.id.textWeightInput);
-                            textIn.setText("");
-                            break;
-                        case 4:
-                            textIn = (EditText) findViewById(R.id.timeInput);
-                            textIn.setText("");
-                            break;
-                        case 5:
-                            textIn = (EditText) findViewById(R.id.textVolumeInput);
-                            textIn.setText("");
-                            break;
-                    }
+                return false;
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                viewPager.setCurrentItem(tab.getPosition());
+
+                switch (tab.getPosition()) {
+                    case 0:
+                        viewPager.setCurrentItem(0);
+                        toolbar.setTitle(R.string.length);
+                        tab.getIcon().setColorFilter(Color.parseColor("#FFFFFF"), PorterDuff.Mode.SRC_IN);
+
+                        break;
+                    case 1:
+                        viewPager.setCurrentItem(1);
+                        toolbar.setTitle(R.string.area);
+                        tab.getIcon().setColorFilter(Color.parseColor("#FFFFFF"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 2:
+                        viewPager.setCurrentItem(2);
+                        toolbar.setTitle(R.string.temp);
+                        tab.getIcon().setColorFilter(Color.parseColor("#FFFFFF"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 3:
+                        viewPager.setCurrentItem(3);
+                        toolbar.setTitle(R.string.weight);
+                        tab.getIcon().setColorFilter(Color.parseColor("#FFFFFF"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 4:
+                        viewPager.setCurrentItem(4);
+                        toolbar.setTitle(R.string.time);
+                        tab.getIcon().setColorFilter(Color.parseColor("#FFFFFF"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 5:
+                        viewPager.setCurrentItem(5);
+                        toolbar.setTitle(R.string.vol);
+                        tab.getIcon().setColorFilter(Color.parseColor("#FFFFFF"), PorterDuff.Mode.SRC_IN);
+                        break;
 
 
                 }
-            });
-            fab.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View view) {
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                tab.getIcon().setColorFilter(Color.parseColor("#1a237e"), PorterDuff.Mode.SRC_IN);
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
 
 
-                    return false;
-                }
-            });
+            }
+        });
 
-            viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-            tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-
-
-                @Override
-                public void onTabSelected(TabLayout.Tab tab) {
-                    InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-                    viewPager.setCurrentItem(tab.getPosition());
-
-                    switch (tab.getPosition()) {
-                        case 0:
-                            viewPager.setCurrentItem(0);
-                            toolbar.setTitle(R.string.length);
-                            tab.getIcon().setColorFilter(Color.parseColor("#FFFFFF"), PorterDuff.Mode.SRC_IN);
-
-                            break;
-                        case 1:
-                            viewPager.setCurrentItem(1);
-                            toolbar.setTitle(R.string.area);
-                            tab.getIcon().setColorFilter(Color.parseColor("#FFFFFF"), PorterDuff.Mode.SRC_IN);
-                            break;
-                        case 2:
-                            viewPager.setCurrentItem(2);
-                            toolbar.setTitle(R.string.temp);
-                            tab.getIcon().setColorFilter(Color.parseColor("#FFFFFF"), PorterDuff.Mode.SRC_IN);
-                            break;
-                        case 3:
-                            viewPager.setCurrentItem(3);
-                            toolbar.setTitle(R.string.weight);
-                            tab.getIcon().setColorFilter(Color.parseColor("#FFFFFF"), PorterDuff.Mode.SRC_IN);
-                            break;
-                        case 4:
-                            viewPager.setCurrentItem(4);
-                            toolbar.setTitle(R.string.time);
-                            tab.getIcon().setColorFilter(Color.parseColor("#FFFFFF"), PorterDuff.Mode.SRC_IN);
-                            break;
-                        case 5:
-                            viewPager.setCurrentItem(5);
-                            toolbar.setTitle(R.string.vol);
-                            tab.getIcon().setColorFilter(Color.parseColor("#FFFFFF"), PorterDuff.Mode.SRC_IN);
-                            break;
-
-
-                    }
-                }
-
-                @Override
-                public void onTabUnselected(TabLayout.Tab tab) {
-                    tab.getIcon().setColorFilter(Color.parseColor("#1a237e"), PorterDuff.Mode.SRC_IN);
-                }
-
-                @Override
-                public void onTabReselected(TabLayout.Tab tab) {
-
-
-                }
-            });
-
-        }
-        }
+    }
+}
